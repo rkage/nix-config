422850f231bcb29debe61c4b71fad6ffcf353bf8
Nick M <4718+rkage@users.noreply.github.com>
2024-06-23 08:39:41 -0400

    cleanup nvim

--- home/nick/shared/nvim/default.nix
+++ home/nick/shared/nvim/default.nix
@@ -1,26 +1,20 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  home.sessionVariables.EDITOR = "nvim";
-
-  home.packages = with pkgs; [
-    nodejs
-    nixd
-    alejandra
-    nixfmt-rfc-style
-    nix-diff
-    gcc
-  ];
-
+{pkgs, ...}: {
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
+    defaultEditor = true;
+    extraPackages = with pkgs; [
+      nodejs
+      nixd
+      alejandra
+      nixfmt-rfc-style
+      nix-diff
+      gcc
+    ];
   };
 
-  xdg.configFile."nvim" = {
+  xdg.configFile.nvim = {
     source = ./nvchad;
     recursive = true;
   };
